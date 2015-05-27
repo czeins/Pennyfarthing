2c12877199a0361471ab69b0c36e099ef3fe9b46
Rachel Okimoto <rachel.okimoto@gmail.com>
2015-05-27 15:47:24 -0700

    worked on scrolling issues

--- www/js/applicationcontroller.js
+++ www/js/applicationcontroller.js
@@ -11,7 +11,7 @@
 
       self.groups = [];
 
-      for (var i=0; i<5; i++) {
+      for (var i=0; i<15; i++) {
         self.groups[i] = {
           title: "Title " + i,
           content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed tempor dolor, in sodales sapien. Vivamus tortor augue, feugiat a mattis et, venenatis sit amet elit. In hac habitasse platea dictumst. Vivamus varius blandit nulla sodales imperdiet. Aliquam nec ex ac nulla mollis facilisis. Morbi elementum laoreet leo, eu maximus nunc porta eget. Vestibulum nec metus libero. Proin vel lacus consectetur, ultricies ex at, molestie augue. Morbi blandit, augue ac condimentum pellentesque, tortor mi fringilla dolor, a lacinia lacus libero in velit. Sed auctor, eros id euismod sodales, quam massa scelerisque dolor, sed condimentum elit risus ac orci. Suspendisse viverra lobortis egestas. Morbi varius lacinia ex at feugiat. Nunc sed accumsan mi, a vehicula libero. Aliquam convallis vehicula nisi, eget venenatis ligula tincidunt in. Nunc mattis facilisis tellus, in condimentum sem vulputate vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
@@ -34,11 +34,18 @@
 
       // scroll function
       self.scrollToTop = function(elementId) {
-        var height = elementId * 52 + 1;
-        $ionicScrollDelegate.scrollTo(0, height);
 
+        var height = elementId * 52 + (1 * elementId);
+
+        function scroll(height) {
+          $ionicScrollDelegate.scrollTo(0, height);
+        }
+
+        setTimeout(scroll(height), 2000);
+
+        // $location.hash(elementId);
+        // $ionicScrollDelegate.scrollTop();
       };
-
 
       // menu that pops up
       $ionicModal.fromTemplateUrl('my-modal.html', {
