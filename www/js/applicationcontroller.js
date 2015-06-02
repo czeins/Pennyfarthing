59e3c83c20283964a4572c697fe48095d12b824c
Rachel Okimoto <rachel.okimoto@gmail.com>
2015-06-02 16:30:59 -0700

    Fixed accordion and pulled API

--- www/js/applicationcontroller.js
+++ www/js/applicationcontroller.js
@@ -11,13 +11,45 @@
       var hamburgerSelected = false;
       self.groups = [];
 
-      for (var i=0; i<15; i++) {
-        self.groups[i] = {
-          title: "Title " + i,
-          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed tempor dolor, in sodales sapien. Vivamus tortor augue, feugiat a mattis et, venenatis sit amet elit. In hac habitasse platea dictumst. Vivamus varius blandit nulla sodales imperdiet. Aliquam nec ex ac nulla mollis facilisis. Morbi elementum laoreet leo, eu maximus nunc porta eget. Vestibulum nec metus libero. Proin vel lacus consectetur, ultricies ex at, molestie augue. Morbi blandit, augue ac condimentum pellentesque, tortor mi fringilla dolor, a lacinia lacus libero in velit. Sed auctor, eros id euismod sodales, quam massa scelerisque dolor, sed condimentum elit risus ac orci. Suspendisse viverra lobortis egestas. Morbi varius lacinia ex at feugiat. Nunc sed accumsan mi, a vehicula libero. Aliquam convallis vehicula nisi, eget venenatis ligula tincidunt in. Nunc mattis facilisis tellus, in condimentum sem vulputate vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          id: i
-        };
+      
+      var html = '';
+      function getHtml() {
+        $http.get('https://fast-bastion-6637.herokuapp.com/api/v1/stories/')
+          .success(function(data){
+         
+          var storyLength = data.length;
+            for (var i=0; i<storyLength; i++) {
+                var html = data[i].html;
+               console.log(data[i].html);
+              self.groups[i] = {
+                title: data[i].title,
+                content: html,
+                id: i,
+                visible: true
+              };
+            }
+
+
+          });
       }
+
+      getHtml();
+
+      var clicked = false;
+
+      self.setVisibility = function(id){
+        if (clicked === false) {
+          for (var q=0; q<self.groups.length; q++) {
+            self.groups[q].id === id ? self.groups[q].visible = true : self.groups[q].visible = false;
+          } 
+          clicked = true;
+        } else {
+            for (var j=0; j<self.groups.length; j++) {
+              self.groups[j].visible = true;
+            }
+          clicked = false;
+          }
+      };
 
       // accordion function
       self.toggleGroup = function(group) {
@@ -53,11 +85,11 @@
 
       // scroll function
       self.scrollToTop = function(elementId) {
-        var height = 120 * (elementId);
+        // var height = 120 * (elementId);
 
-        setTimeout(function() {
-          $ionicScrollDelegate.scrollTo(0, height, true);
-        }, 100);
+        // setTimeout(function() {
+        //   $ionicScrollDelegate.scrollTo(0, height, true);
+        // }, 100);
 
       };
 
